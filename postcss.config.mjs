2b618965b1715edb15bcda96a1fc5fcb4d29415c
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-05-26 21:12:32 -0300

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -4,21 +4,21 @@ import PostcssModulesPlugin from 'postcss-modules'
 import stringHash from 'string-hash'
 
 export default {
-  // plugins: [postcssPresetEnv(), autoprefixer(), PostcssModulesPlugin()],
-  // autoModules: false,
-  // onlyModules: false,
-  // extensions: ['.scss'],
-  // use: ['sass'],
-  // minimize: true,
-  // sourceMap: false,
-  // modules: {
-  //   generateScopedName: (name, filename, css) => {
-  //     if (filename.includes('global')) {
-  //       return name
-  //     }
-  //     const hash = stringHash(css).toString(36).substring(0, 5)
-  //     return `test_${name}_${hash}`
-  //   }
-  // },
-  // extract: 'css/test-library.min.css'
+  plugins: [postcssPresetEnv(), autoprefixer(), PostcssModulesPlugin()],
+  autoModules: false,
+  onlyModules: false,
+  extensions: ['.scss'],
+  use: ['sass'],
+  minimize: true,
+  sourceMap: false,
+  modules: {
+    generateScopedName: (name, filename, css) => {
+      if (filename.includes('global')) {
+        return name
+      }
+      const hash = stringHash(css).toString(36).substring(0, 5)
+      return `test_${name}_${hash}`
+    }
+  },
+  extract: 'css/test-library.min.css'
 }
