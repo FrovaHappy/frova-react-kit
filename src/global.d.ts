18c7a55e1e053d9005b1839872408a485ed6e13d
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-05-27 21:04:49 -0300

    add support to x-title element

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,3 +2,9 @@ declare module '*.module.scss' {
   const content: Record<string, string>
   export default content
 }
+
+namespace JSX {
+  interface IntrinsicElements {
+    'x-title': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
+  }
+}
