378d3d5451d086b7072e64e4390a0f93839c2248
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-06-10 16:35:55 -0300

    add observer position titles

--- src/components/Document/Content.tsx
+++ src/components/Document/Content.tsx
@@ -1,10 +1,35 @@
 import style from '@styles/DocumentMarkdown/content.module.scss'
+import shared from '@styles/DocumentMarkdown/shared.module.scss'
+import React from 'react'
 interface Props {
   html: string
 }
 function Content(props: Props) {
+  const htmlRef = React.useRef<HTMLDivElement>(null)
+  React.useEffect(() => {
+    if (!htmlRef.current) return
+    const options = {
+      root: null,
+      rootMargin: `-100px`,
+      threshold: 1
+    }
+    const handleObserver = (entries: IntersectionObserverEntry[]) => {
+      let active = entries[0].target.id
+
+      const anchor = window.document.querySelector('a[href="#' + active + '"]')
+      anchor?.parentElement?.parentElement?.querySelectorAll('a').forEach(a => {
+        a.classList.remove(shared.active)
+      })
+      anchor?.classList.add(shared.active)
+    }
+
+    const observer = new IntersectionObserver(handleObserver, options)
+    htmlRef.current.querySelectorAll('h1, h2, h3, h4').forEach(el => {
+      observer.observe(el)
+    })
+  }, [htmlRef])
   const { html } = props
-  return <div className={style.content} dangerouslySetInnerHTML={{ __html: html }} />
+  return <div className={style.content} dangerouslySetInnerHTML={{ __html: html }} ref={htmlRef} />
 }
 
 export default Content
