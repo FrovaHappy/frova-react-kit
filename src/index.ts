35bcbf193c8bb67eee0dcda36ad024012c08111f
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-06-13 19:54:46 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import '@styles/variables.global.scss'
 
 export { default as DocMarkdown } from './components/Document'
+export { default as InputContent } from './components/InputContent'
+export { default as InputText } from './components/InputText'
