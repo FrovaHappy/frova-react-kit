42e19b240b383fcb84ee5b0b8cd4573bbc735af0
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-07-03 15:42:18 -0300

    refactor useParseMd

--- apps/package/src/hooks/useParseMd.ts
+++ apps/package/src/hooks/useParseMd.ts
@@ -6,14 +6,265 @@ import remarkRehype from 'remark-rehype'
 import rehypeStringify from 'rehype-stringify'
 import remarkGfm from 'remark-gfm'
 
+const rulesMatches = {
+  ul: /^-\s/,
+  ol: /^\d+\.\s/,
+  blockquote: /^>\s/,
+  tasklist: /^-\s\[([\sx])\]\s/,
+  image: /^!\[([\w\s()!@:%_\+.~#?&\/=-]+)\]\(([\w()!@:%_\+.~#?&\/=-]+)\)/,
+  code: /^```([\w\W]+)/,
+  table: /^\|([\w\s<>"()!@:%_\+.~#?&\/=-]+)\|/
+}
+
+let endBlock = -1
+
+type FormatBlock = {
+  md: string[]
+  startIndex: number
+  match: RegExp
+  remplaceWith: [RegExp, `${string}<contentReplaced>${string}` | string][]
+  contentExternal: string
+}
+
+const formatBlock = (props: FormatBlock) => {
+  const { remplaceWith, contentExternal, startIndex, md, match } = props
+  let textResult = ''
+  for (let j = startIndex; j < md.length; j++) {
+    endBlock = j
+    const text = md[j]
+    if (text.match(match)) {
+      for (const [startWith, textToReplace] of remplaceWith) {
+        if (!text.match(startWith)) continue
+        if (textToReplace.includes('<contentReplaced>')) {
+          textResult += textToReplace.replace('<contentReplaced>', text.replace(startWith, ''))
+        } else {
+          textResult += text.replace(startWith, textToReplace)
+        }
+      }
+    } else {
+      break
+    }
+  }
+  textResult = contentExternal.replace('<contentReplaced>', textResult)
+  return textResult
+}
+
+const formatCode = (props: FormatBlock) => {
+  const { remplaceWith, contentExternal, startIndex, md, match } = props
+  const [[startWith, replace]] = remplaceWith
+  let textResult = md[startIndex].replace(startWith, replace)
+
+  let index = startIndex + 1
+  while (!md[index]?.match(match) && index < md.length) {
+    const text = md[index]
+    textResult += text + '\n'
+    index++
+  }
+  endBlock = index
+  textResult += contentExternal
+  return textResult
+}
+type FormatTable = {
+  md: string[]
+  startIndex: number
+}
+const formatTable = (props: FormatTable) => {
+  const { md, startIndex } = props
+  let index = startIndex
+  const tableRegex = rulesMatches.table
+  const cellsContent = (i: number) => {
+    const result: string[] = []
+    let text = md[i]
+    let match = text.match(tableRegex)
+    while ((match = text.match(tableRegex)) !== null) {
+      result.push(match[1].trim())
+      text = text.replace('|', '').replace(match[1], '')
+    }
+    return result
+  }
+  const tableHeader: string[] = cellsContent(index)
+  index += 1 // Skip header line
+  const align: Array<string | undefined> = (() => {
+    const result: typeof align = []
+    const alignRegex = /\|(\s+:?-+:?\s+)\|/
+    let text = md[index]
+    let match = text.match(alignRegex)
+    if (!match) return result
+    while ((match = text.match(alignRegex)) !== null) {
+      const aligns = {
+        left: text.match(/^\|\s+:-+\s+\|/),
+        right: text.match(/^\|\s+-+:\s+\|/),
+        center: text.match(/^\|\s+:-+:\s+\|/)
+      }
+      const indexAligns = Object.values(aligns).findIndex(v => {
+        console.log(v)
+        return v
+      })
+      if (indexAligns === -1) {
+        console.log('none')
+        result.push(undefined)
+      } else {
+        console.log(Object.keys(aligns)[indexAligns])
+        result.push(Object.keys(aligns)[indexAligns])
+      }
+      text = text.replace('|', '').replace(match[1], '')
+    }
+
+    console.log(result)
+    return result
+  })()
+  index += 1 // skip align line
+
+  const tableBody: string[][] = []
+  for (let i = index; i < md.length; i++) {
+    if (!md[i].match(tableRegex)) break
+    tableBody.push(cellsContent(i))
+    index++
+  }
+  endBlock = index
+  return `<table>
+<thead>
+<tr>
+${tableHeader.map((header, i) => `<th${align[i] ? ` align="${align[i]}"` : ''}>${header}</th>`).join('\n')}
+</tr>
+</thead>
+<tbody>
+${tableBody
+  .map(
+    row =>
+      `<tr>\n${row.map((cell, i) => `<td${align[i] ? ` align="${align[i]}"` : ''}>${cell}</td>`).join('\n')}\n</tr>`
+  )
+  .join('\n')}
+</tbody>
+</table>`
+}
+
 export default async function useParseMd(str: string) {
-  const md = await remark()
-    .use(remarkParse)
-    .use(remarkHeadingId, { defaults: true })
-    .use(remarkGfm)
-    .use(remarkRehype)
-    .use(rehypeHighlight)
-    .use(rehypeStringify)
-    .process(str)
-  return md.value.toString()
+  endBlock = -1
+  const md = str.split('\n')
+  const result: string[] = []
+  for (let i = 0; i < md.length; i++) {
+    if (endBlock >= i) continue
+    let line = md[i]
+    if (line.match(rulesMatches.table)) {
+      const text = formatTable({
+        md,
+        startIndex: i
+      })
+      result.push(text)
+      continue
+    }
+    if (line.startsWith('#')) {
+      const count = line.split('#').length - 1
+      const text = line.replace(/^#+\s*/, '')
+      result.push(`<h${count} id="${text.replaceAll(' ', '-')}">${text}</h${count}>`)
+      continue
+    }
+
+    if (line.match(rulesMatches.code)) {
+      const text = formatCode({
+        md,
+        startIndex: i,
+        match: /```/,
+        remplaceWith: [[rulesMatches.code, '<pre><code class="language-$1">']],
+        contentExternal: '</code></pre>'
+      })
+      result.push(text)
+      continue
+    }
+    if (line.match(rulesMatches.blockquote)) {
+      const text = formatBlock({
+        md,
+        startIndex: i,
+        match: rulesMatches.blockquote,
+        remplaceWith: [[rulesMatches.blockquote, '<p><contentReplaced></p>']],
+        contentExternal: '<blockquote><contentReplaced></blockquote>'
+      })
+      result.push(text)
+      continue
+    }
+    if (line.match(rulesMatches.tasklist)) {
+      const text = formatBlock({
+        md,
+        startIndex: i,
+        match: rulesMatches.tasklist,
+        remplaceWith: [
+          [/-\s\[\s\]\s/, '<li><input type="checkbox" disabled><contentReplaced></li>'],
+          [/-\s\[x\]\s/, '<li><input type="checkbox" checked disabled><contentReplaced></li>']
+        ],
+        contentExternal: '<ul><contentReplaced></ul>'
+      })
+      result.push(text)
+      continue
+    }
+    if (line.match(rulesMatches.ul)) {
+      const text = formatBlock({
+        md,
+        startIndex: i,
+        match: rulesMatches.ul,
+        remplaceWith: [[rulesMatches.ul, '<li><contentReplaced></li>']],
+        contentExternal: '<ul><contentReplaced></ul>'
+      })
+      result.push(text)
+      continue
+    }
+    if (line.match(rulesMatches.ol)) {
+      const text = formatBlock({
+        md,
+        startIndex: i,
+        match: rulesMatches.ol,
+        remplaceWith: [[rulesMatches.ol, '<li><contentReplaced></li>']],
+        contentExternal: '<ol><contentReplaced></ol>'
+      })
+      result.push(text)
+      continue
+    }
+    if (line.match(rulesMatches.image)) {
+      const text = formatBlock({
+        md,
+        startIndex: i,
+        match: rulesMatches.image,
+        remplaceWith: [[rulesMatches.image, '<img src="$2" alt="$1">']],
+        contentExternal: '<p><contentReplaced></p>'
+      })
+      result.push(text)
+      continue
+    }
+    if (line.startsWith('---')) {
+      result.push(`<hr>`)
+      continue
+    }
+    if (line.length === 0) {
+      result.push('')
+      continue
+    }
+
+    // Style in line
+    const regInline: Record<string, [RegExp, string]> = {
+      code: [/`([\w\W]+?[^`])`/, `<code>$1</code>`],
+      strong: [/[\*]{2}(([\w\W]+?[^*]))[\*]{2}/, '<strong>$1</strong>'],
+      italic: [/\*(([\w\W]+?[^*]))\*/, '<em>$1</em>'],
+      image: [/\!\[([\w\s()!@:%_\+.~#?&\/=-]+)\]\(([\w()!@:%_\+.~#?&\/=-]+)\)/, '<img src="$2" alt="$1">'],
+      anchor: [/\[([\w\s<>"()!@:%_\+.~#?&\/=-]+)\]\(([\w()!@:%_\+.~#?&\/=-]+)\)/, '<a href="$2">$1</a>']
+    }
+    Object.values(regInline).forEach(([reg, replace]) => {
+      while (line.match(reg)?.length ?? 0 > 0) {
+        line = line.replace(reg, replace)
+      }
+    })
+
+    result.push(`<p>${line}</p>`)
+  }
+
+  return result.join('\n')
+
+  // const md = await remark()
+  //   .use(remarkParse)
+  //   .use(remarkHeadingId, { defaults: true })
+  //   .use(remarkGfm)
+  //   .use(remarkRehype)
+  //   .use(rehypeHighlight)
+  //   .use(rehypeStringify)
+  //   .process(str)
+  // return md.value.toString()
 }
--- apps/package/test/hooks/useParseMd.test.ts
+++ apps/package/test/hooks/useParseMd.test.ts
@@ -4,27 +4,27 @@ const Rules = {
   empty: { title: 'should return a html string with empty', expect: ['', ''] },
   h1: {
     title: 'should return a html string with h1',
-    expect: ['# hello-h1', '<h1 id="hello-h-1">hello-h1</h1>']
+    expect: ['# hello-h1', '<h1 id="hello-h1">hello-h1</h1>']
   },
   h2: {
     title: 'should return a html string with h2',
-    expect: ['## hello h2', '<h2 id="hello-h-2">hello h2</h2>']
+    expect: ['## hello h2', '<h2 id="hello-h2">hello h2</h2>']
   },
   h3: {
     title: 'should return a html string with h3',
-    expect: ['### hello h3', '<h3 id="hello-h-3">hello h3</h3>']
+    expect: ['### hello h3', '<h3 id="hello-h3">hello h3</h3>']
   },
   h4: {
     title: 'should return a html string with h4',
-    expect: ['#### hello h4', '<h4 id="hello-h-4">hello h4</h4>']
+    expect: ['#### hello h4', '<h4 id="hello-h4">hello h4</h4>']
   },
   h5: {
     title: 'should return a html string with h5',
-    expect: ['##### hello h5', '<h5 id="hello-h-5">hello h5</h5>']
+    expect: ['##### hello h5', '<h5 id="hello-h5">hello h5</h5>']
   },
   h6: {
     title: 'should return a html string with h6',
-    expect: ['###### hello h6', '<h6 id="hello-h-6">hello h6</h6>']
+    expect: ['###### hello h6', '<h6 id="hello-h6">hello h6</h6>']
   },
   p: {
     title: 'should return a html string with p',
@@ -32,24 +32,21 @@ const Rules = {
   },
   code: {
     title: 'should return a html string with code',
-    expect: [
-      '```js\nconst a = 1\n```',
-      '<pre><code class="hljs language-js"><span class="hljs-keyword">const</span> a = <span class="hljs-number">1</span>\n</code></pre>'
-    ]
+    expect: ['```js\nconst a = 1\n```', '<pre><code class="language-js">const a = 1\n</code></pre>']
   },
   blockquote: {
     title: 'should return a html string with blockquote',
-    expect: ['> hello blockquote', '<blockquote>\n<p>hello blockquote</p>\n</blockquote>']
+    expect: ['> hello blockquote\n> line2', '<blockquote><p>hello blockquote</p><p>line2</p></blockquote>']
   },
   ol: {
     title: 'should return a html string with ol',
-    expect: ['1. hello ol \n2. hello ol', '<ol>\n<li>hello ol</li>\n<li>hello ol</li>\n</ol>']
+    expect: ['1. hello ol\n2. hello ol', '<ol><li>hello ol</li><li>hello ol</li></ol>']
   },
   tasklist: {
     title: 'should return a html string with tasklist',
     expect: [
-      '- [ ] hello tasklist \n- [x] hello tasklist',
-      '<ul class="contains-task-list">\n<li class="task-list-item"><input type="checkbox" disabled> hello tasklist</li>\n<li class="task-list-item"><input type="checkbox" checked disabled> hello tasklist</li>\n</ul>'
+      '- [ ] hello tasklist\n- [x] hello tasklist',
+      '<ul><li><input type="checkbox" disabled>hello tasklist</li><li><input type="checkbox" checked disabled>hello tasklist</li></ul>'
     ]
   },
   table: {
@@ -61,7 +58,7 @@ const Rules = {
   },
   ul: {
     title: 'should return a html string with ul',
-    expect: ['- hello ul \n- hello ul', '<ul>\n<li>hello ul</li>\n<li>hello ul</li>\n</ul>']
+    expect: ['- hello ul\n- hello ul', '<ul><li>hello ul</li><li>hello ul</li></ul>']
   },
   img: {
     title: 'should return a html string with img',
@@ -78,7 +75,7 @@ const Rules = {
     title: 'should return a html string with imgs',
     expect: [
       '![alt](https://example.com/image.png)\n![alt](https://example.com/image.png)',
-      '<p><img src="https://example.com/image.png" alt="alt">\n<img src="https://example.com/image.png" alt="alt"></p>'
+      '<p><img src="https://example.com/image.png" alt="alt"><img src="https://example.com/image.png" alt="alt"></p>'
     ]
   },
   link: {
