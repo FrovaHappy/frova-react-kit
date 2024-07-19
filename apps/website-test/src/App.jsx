e3cb3b6ef46ad1fdd15d24db2f33bc90dd6c144b
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-07-19 17:43:21 -0300

    chore(app-test): :fire: delete code related with buttons modules
    will be integrated in future features

--- apps/website-test/src/App.jsx
+++ apps/website-test/src/App.jsx
@@ -1,34 +1,10 @@
-import { DocMarkdown, InputContent, InputText } from 'frova-ui'
+import { DocMarkdown } from 'frova-ui'
 import './App.css'
 import 'highlight.js/styles/github-dark.css'
 
 function App() {
   return (
     <>
-      <div className='input-container'>
-        <InputText title='color' placeholder='Title' type='color' />
-        <InputText title='number' placeholder='Title' type='number' />
-        <InputText title='search' placeholder='Title' type='search' />
-        <InputText title='date' placeholder='Title' type='date' />
-        <InputText title='datetime-local' placeholder='Title' type='datetime-local' />
-        <InputText title='url' placeholder='Title' type='url' />
-        <InputText title='email' placeholder='Title' type='email' />
-        <InputText title='tel' placeholder='Title' type='tel' />
-        <InputText title='password' placeholder='Title' type='password' />
-        <InputText
-          title='Title 2'
-          placeholder='Title'
-          width='min-content'
-          onChange={e => console.log(e.target.value)}
-        />
-        <InputContent
-          title='Content'
-          placeholder='Content'
-          value='Beatriz Bot is a bot that can help you to create a markdown file from a text input.'
-          onChange={e => console.log(e.target.value)}>
-          description
-        </InputContent>
-      </div>
       <DocMarkdown
         articles={[
           {
