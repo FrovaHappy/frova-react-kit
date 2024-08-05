6f74affb7344fbd192700bc4ee9c15acbf37d40a
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-08-05 19:18:31 -0300

    refactor(package): :white_check_mark: test refactoring

--- apps/package/src/hooks/useFetchMd.ts
+++ apps/package/src/hooks/useFetchMd.ts
@@ -1,14 +1,20 @@
+export const ErrorMessages = {
+  noFound: 'fetch: File not found',
+  fetch: 'fetch: Error fetching file',
+  contentType: 'fetch: content type is not text/plain'
+}
+
 export default async function useFetchMd(url: string) {
   const file = await fetch(url).catch(() => {
-    throw new Error('fetch: File not found')
+    throw new Error(ErrorMessages.noFound)
   })
   if (!file.ok) {
-    throw new Error('fetch: Error fetching file')
+    throw new Error(ErrorMessages.fetch)
   }
   const headerType = file.headers.get('content-type')
   if (!(headerType?.includes('text/plain') || headerType?.includes('text/markdown'))) {
     console.warn(file.headers.get('content-type'))
-    throw new Error('fetch: content type is not text/plain')
+    throw new Error(ErrorMessages.contentType)
   }
 
   return await file.text()
--- apps/package/test/MarkDownDoc/Main.test.tsx
+++ apps/package/test/MarkDownDoc/Main.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen, waitFor } from '@testing-library/react'
 import Main from '@components/Document'
 import articles from '@mocks/articles'
+import { ErrorMessages } from '@/hooks/useFetchMd'
 describe('test Main Component', () => {
   test('loading md component', async () => {
     render(<Main articles={articles} />)
@@ -11,13 +12,13 @@ describe('test Main Component', () => {
     articles[0].sections[0].url = ''
     render(<Main articles={articles} />)
 
-    const loading = await waitFor(() => screen.getByText('fetch: File not found'), { timeout: 3000 })
+    const loading = await waitFor(() => screen.getByText(ErrorMessages.noFound, { selector: 'div' }), { timeout: 3000 })
 
     // check if the Nav component is rendered
     const navTitle = screen.getByText(articles[0].title, { selector: 'x-title' })
     expect(navTitle).not.toBeTypeOf('undefined')
 
     // check if the error message is rendered
-    expect(loading.innerHTML).toBe('fetch: File not found')
+    expect(loading.innerHTML).toContain(ErrorMessages.noFound)
   })
 })
